e64a0088b0f4021fb21304765df3e3c33a760745
Martin Jego Alberto <martsalberto@gmail.com>
2018-07-30 21:33:54 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Topic from './components/Topic';
+import Topics from './components/Topics';
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+    </div>
+  </Router>
+)
+export default BasicExample
